90809bd352c2d8a038b555e13ff035ebb9debd00
ejanalysis <ejanalysis@users.noreply.github.com>
2018-03-14 17:56:45 -0400

    PR is no longer in default for clean.mystates

--- R/clean.mystates.R
+++ R/clean.mystates.R
@@ -15,11 +15,11 @@
 #' @param mystates Character vector, optional. 
 #'   Defines which states, using 2-character abbreviations (case-insensitive), or 'all' for all available.
 #'   
-#'   Default is 'all' which is 50 states plus DC plus PR (but not AS, GU, MP, UM, VI, US).
+#'   Default is 'all' which is 50 states plus DC, BUT NO LONGER PR (& not AS, GU, MP, UM, VI, US).
 #'   If mystates is not specified or is 'all' then this returns the default.
 #'   If mystates is specified, returns those defaults that match any of mystates.
 #'   If mystates includes DC, PR, or US, those are returned, but "AS" "GU" "MP" "UM" "VI" are removed.
-#'   One element of the vector can be 50, 51, or 52, which represents the 50 states, those 50 plus DC, or the 51 plus PR. 
+#'   One element of the vector can be 50, 51, or 52, where 50 represents the 50 states, 51 = 50 plus DC, or 52 = the 51 plus PR.
 #'   In other words, the default could be written as c(50,'DC','PR') or as c(51,'PR') or just 52.
 #'   Redundant entries are dropped, e.g., c(51,'DC') becomes 51.
 #' 
@@ -27,34 +27,37 @@
 #' @return Returns character vector of 2-character State abbreviations, lower case.
 #' @seealso \code{\link{get.acs}} and \code{\link{download.datafiles}} which use this, and \code{\link[ejanalysis]{get.state.info}} (from \pkg{ejanalysis} package) based on \code{\link[proxistat]{lookup.states}} or \code{data(lookup.states, package=proxistat)} using \pkg{proxistat} package
 #' @export
-clean.mystates <- function(mystates='all', testing=FALSE) {
-
+clean.mystates <- function(mystates = 'all', testing = FALSE) {
+  
   #stateinfo	<- ejanalysis::get.state.info()
   # or could use this:
-  data(lookup.states, envir = environment(), package='proxistat');   stateinfo <- lookup.states
+  data(lookup.states, envir = environment(), package = 'proxistat');   stateinfo <- lookup.states
   # or state.abb from base datasets but that lacks DC PR VI GU etc.
   # also,
   #statenames	<- stateinfo$ftpname
   allabbs	<- tolower(stateinfo$ST)
   stateabbs <- allabbs[!(allabbs %in% c("as", "gu", "mp", "um", "vi", "us"))]
-  # that leaves 52 (states+dc+pr) 
+  # that leaves 52 (states+dc+pr)
+  # *** now remove pr
+  stateabbs <- stateabbs[stateabbs != 'pr']
   
   mystates <- tolower(mystates)
   
   # expand states specified by 50,51,52,'all'
-  if ('all' %in% mystates) {mystates <- c(mystates[mystates!='all'], stateabbs)}
-  if (50 %in% mystates) {mystates <- c(mystates[mystates!=50], allabbs[1:51]); mystates <- mystates[mystates!='dc']}
-  if (51 %in% mystates) {mystates <- c(mystates[mystates!=51], allabbs[1:51])}
-  if (52 %in% mystates) {mystates <- c(mystates[mystates!=52], allabbs[1:51], 'pr')}
+  x51 <- allabbs[1:51]
+  if ('all' %in% mystates) {mystates <- c(mystates[mystates != 'all'], stateabbs)}
+  if (50 %in% mystates)    {mystates <- c(mystates[mystates != 50], x51[x51 != 'dc'])}
+  if (51 %in% mystates)    {mystates <- c(mystates[mystates != 51], x51)}
+  if (52 %in% mystates)    {mystates <- c(mystates[mystates != 52], x51, 'pr')}
   
   # remove duplicates:
   mystates <- unique(mystates)
   
   if (!all(mystates %in% allabbs)) {stop('not all specified states were found in list of state abbreviations')}
-
+  
   #  remove island areas/territories ( but not  'us'?)  :
   mystates <- mystates[ mystates %in% c('us', stateabbs) ]
-
+  
   if (testing) { mystates	<- c("dc", "de") }
   
   return(mystates)
