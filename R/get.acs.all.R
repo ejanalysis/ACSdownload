98856b1bff3912c251f27357d9e5838c690637a9
Mark Corrales <corrales.mark@epa.gov>
2022-05-31 00:04:31 -0400

    get.acs.all() created, to get quickly all blockgroups in USA for selected tables

---
+++ R/get.acs.all.R
@@ -0,0 +1,102 @@
+#' get ACS summaryfile specified tables for entire USA, all blockgroups or all tracts
+#'
+#' Uses Census Bureau new format files that offer the full USA in 1 file per table,
+#'   via FTP or https. 
+#'   
+#' @param tables 'ejscreen' or else one or more table codes like 'b01001' or c("B01001", "B03002")
+#' @param end.year 2020 or another available year
+#' @param dataset default is 5, for 5-year summary file data like 2016-2020 ACS
+#' @param sumlevel default is 150 for blockgroups. 140 is tracts.
+#' @param output.path folder / path where to save the downloaded files 
+#'
+#' @return list of data.frames, one per table requested
+#' @export
+#'
+get.acs.all <- function(tables="B01001", end.year=2020, dataset='5', sumlevel=150, output.path = file.path('~', 'acsoutput') ) {
+  # new format for 5yr summary file ACS
+  # makes it easier to get one table for every blockgroup in the US
+  
+  # table_for_sumlevel
+  ejscreentables <- c("B01001", "B03002", "B15002", "B16002", "C17002", "B25034")
+  # if tables equals or contains 'ejscreen' then replace the 'ejscreen' part with the default tables as follows, leaving any other tables specified in addition to ejscreen tables:
+  if (any(tolower(tables) == 'ejscreen')) {
+    tables <- c(ejscreentables, tables[tolower(tables) != 'ejscreen'])
+  }
+  # end.year = acsdefaultendyearhere_func()
+  end.year <- as.character(end.year)
+  if (length(end.year) != 1) {stop('end.year must be a single value')}
+  thisyear <- data.table::year(Sys.Date())
+  if (!(end.year %in% as.character(2016:(thisyear - 1)))) {stop('end.year must be a plausible year')}
+  
+  # python example:
+  #   https://github.com/uscensusbureau/acs-summary-file/blob/master/Python/example2.py
+  #   https://github.com/uscensusbureau/acs-summary-file/wiki/Python-Table-Data-for-All-Tracts
+  
+  # 'https://www2.census.gov/programs-surveys/acs/summary_file/2020/prototype/5YRData/'
+  # ftpsite <- "ftp2.census.gov"
+  # list_files_ftp('ftp://ftp2.census.gov/acs_latest_data/summary_file/2020/prototype/5YRData')
+  # 'https://www2.census.gov/programs-surveys/acs/summary_file/2020/prototype/5YRData/acsdt5y2020-b01001.dat'
+  
+  baseurl <- 'ftp://ftp2.census.gov/acs_latest_data/summary_file/{year}/prototype/{dataset}YRData'
+  baseurl <- gsub('\\{year\\}', end.year, baseurl)
+  baseurl <- gsub('\\{dataset\\}', dataset, baseurl)
+  httpurl <- gsub('ftp://ftp2', 'https://www2', baseurl)
+  
+  files <- list_files_ftp(baseurl)
+  files <- grep(pattern = '\\.dat$', x=files, value=TRUE) # almost all the files are .dat
+  # files provides full paths including filename
+  
+  if (tables != '*') {
+    wanted <- paste0(baseurl, '/acsdt5y', end.year, '-', tolower(tables), '.dat')
+    # fullpath incl filename
+    
+    notfound <- setdiff(wanted, files)
+    if (length(notfound) > 0) {message(paste0('Not found: ', paste(notfound, collapse = ', ')))}
+    wanted <- intersect(wanted, files) # just try to download the available ones
+  } else {
+    # keep them all
+  }
+  
+  if (!dir.exists(output.path)) {dir.create(output.path)}
+  
+  wanted_https <- gsub('ftp://ftp2', 'https://www2', wanted)
+  x <- list()
+  for (i in seq_along(wanted)) {
+    # but note (http faster than ftp)
+    #
+    # this could be more robust code, check for failed download and retry, etc.
+    # downloading part seems very slow !  >6 minutes for all US blockgroupsu for just 1 table, reading from file as it downloads
+    # but read_delim() is very fast and read.delim is good enough.
+    # x[[i]] <- readr::read_delim(wanted[i], delim = '|') 
+    
+    x[[i]] <- read.delim(wanted[i], sep = '|')
+    
+    # or if already downloaded: 
+    # x[[i]] <- read.delim(file.path(output.path, gsub(paste0(baseurl,'/'),'', wanted[i])), sep = '|')
+    
+
+    # x <- try(
+    #   download.file(
+    #     # url = wanted_https[i],  # does not work via https, just ftp for some reason
+    #     url = wanted[i],
+    #     destfile = file.path(output.path, gsub(paste0(baseurl,'/'),'', wanted[i]))
+    #   )
+    # )
+    
+    # retain only blockgroups if sumlevel is 150, tracts if it is 140.
+    x[[i]] <- x[[i]][substr(x[[i]][, 'GEO_ID'], 1, 3) == sumlevel, ] 
+   
+    x[[i]]$FIPS   <- substr(x[[i]][, 'GEO_ID'], 10, 21)
+    
+    readr::write_csv(x[[i]], file = paste0(tables[i], '.csv')) # should really ensure this is same as current table
+  }
+
+# these files do include PR and DC but no Island Territories.
+
+  # assumes all were found: 
+  names(x) <- tolower(tables)
+  save(x, file.path(output.path, 'acsdata.rda'))
+  invisible(x)
+}
+
+
---
+++ R/list_files_ftp.R
@@ -0,0 +1,63 @@
+list_files_ftp <- function(url, credentials = "", sleep = NA, sort = FALSE, verbose = FALSE) {
+  
+  # Do
+  x <- url %>%
+    purrr::map(
+      ~list_files_ftp_worker(
+        url = .,
+        credentials = credentials,
+        sleep = sleep,
+        verbose = verbose
+      )
+    ) %>%
+    purrr::flatten_chr()
+  
+  # Sort remote file names
+  if (sort) x <- sort(x)
+  
+  return(x)
+  
+}
+
+
+list_files_ftp_worker <- function(url, credentials, sleep, verbose) {
+  
+  # Message to user
+  if (verbose) message(date_message(), "`", url, "`...")
+  
+  # url must be prefixed with ftp or sftp
+  if (!grepl("^ftp://|^sftp://", url)) {
+    stop("URL must be prefixed with 'ftp://' or 'sftp://'", call. = FALSE)
+  }
+  
+  # Ensure the directory has a trailing separator
+  url <- stringr::str_c(url, .Platform$file.sep)
+  
+  # Get the file list
+  # If credentials are blank, this will still work
+  file_list <- tryCatch({
+    
+    RCurl::getURL(
+      url,
+      userpwd = credentials,
+      ftp.use.epsv = FALSE,
+      dirlistonly = TRUE,
+      forbid.reuse = TRUE,
+      .encoding = "UTF-8"
+    )
+    
+  }, error = function(e) {
+    as.character()
+  })
+  
+  # Make a vector
+  if (length(file_list) != 0) {
+    file_list <- stringr::str_c(url, stringr::str_split(file_list, "\n")[[1]])
+    file_list <- stringr::str_trim(file_list)
+  }
+  
+  if (!is.na(sleep[1])) Sys.sleep(sleep)
+  
+  return(file_list)
+  
+}
