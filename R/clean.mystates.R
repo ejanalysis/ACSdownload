235bcd06285d6c6d807a68d6f40ed166110feb37
Mark Corrales <corrales.mark@epa.gov>
2022-11-06 13:46:20 -0500

    notes

--- R/clean.mystates.R
+++ R/clean.mystates.R
@@ -8,10 +8,31 @@
 #'  #53         60 AS              American Samoa               <NA>\cr
 #'  #54         66 GU                        Guam               <NA>\cr
 #'  #55         69 MP    Northern Mariana Islands               <NA>\cr
-#'  #57         74 UM U.S. Minor Outlying Islands               <NA>\cr
 #'  #58         78 VI         U.S. Virgin Islands               <NA>\cr
+#'  #57         74 UM U.S. Minor Outlying Islands               <NA>\cr
+#'
 #'  Note: The function stops if any of mystates is not found in the full list that includes these,
 #'  but it returns only those excluding these five above, so an invalid state like 'ZQ' causes an error but an invalid state like 'VI' is silently removed without any error.
+#'
+#'  Puerto Rico (PR, FIPS 72), the District of Columbia (DC, 11), and the 50 States are in ACS 2016-2020 and Census 2020.
+#'  For info on Island Areas, see https://www.census.gov/programs-surveys/decennial-census/technical-documentation/island-areas-censuses.html
+#'  The 2020 Island Areas Censuses (IAC) include data for American Samoa (AS, FIPS 60), Guam (GU, 66),
+#'  the Commonwealth of the Northern Mariana Islands (MP, 69), and the U.S. Virgin Islands (VI. 78),
+#'  but not U.S. Minor Outlying Islands (UM, 74).
+#'  The 2020 Census counted people living in the U.S. Island Areas using a long-form questionnaire. Other
+#'  surveys, such as the American Community Survey (ACS), are not conducted in the Island Areas. Therefore,
+#'  the Census Bureau used a long-form questionnaire to meet the Island Areas’ data needs for demographic,
+#'  social, economic, and housing unit information. This long-form questionnaire was similar to the ACS
+#'  questionnaire used in the 50 states, the District of Columbia, and Puerto Rico.
+#'  The 2020 IAC Demographic Profile Summary File provides access to all of the data.
+#'  It provides data down to the place, county subdivision, and estate (USVI only) level - not block groups.
+#'  The 2020 IAC Demographic Profile Summary File data are available through
+#'  the Census Bureau’s data exploration platform, data.census.gov.
+#'  The 2020 IAC Demographic Profile Summary File is located on the U.S. Census Bureau’s file transfer protocol
+#'  (FTP) server at <https://www2.census.gov/programs-surveys/decennial/2020/data/island-areas/.
+#'  The easiest way is to start at the 2020 Island Areas Censuses Data Products webpage at
+#'  www.census.gov/programs-surveys/decennial-census/decade/2020/planning-management/release/2020-islandareas-data-products.html
+#'
 #' @param mystates Character vector, optional.
 #'   Defines which states, using 2-character abbreviations (case-insensitive), or 'all' for all available.
 #'
@@ -30,8 +51,8 @@
 clean.mystates <- function(mystates = 'all', testing = FALSE) {
   #stateinfo	<- ejanalysis::get.state.info()
   # or could use this:
-  data(lookup.states, envir = environment(), package = 'proxistat')
-  stateinfo <- lookup.states
+  # data(lookup.states, envir = environment(), package = 'proxistat')
+  stateinfo <- proxistat::lookup.states
   # or state.abb from base datasets but that lacks DC PR VI GU etc.
   # also,
   #statenames	<- stateinfo$ftpname
